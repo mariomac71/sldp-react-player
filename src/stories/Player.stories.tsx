1eb91c7e9c4fce1d03c4b4395fb9dbee0a1539d0
Fabrizio Armango <fabrizio.armango@gmail.com>
2023-04-19 18:25:57 +0200

    migrated storybook to v7
    Co-authored-by: lachezar-bozhilov <lachezar-bozhilov@users.noreply.github.com>

--- src/stories/Player.stories.tsx
+++ src/stories/Player.stories.tsx
@@ -1,39 +1,41 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import Player from '../Player';
 
-export default {
+const meta: Meta<typeof Player> = {
   title: 'SLDP Player/Player',
   component: Player,
   parameters: {
     layout: 'centered',
-  },
-} as ComponentMeta<typeof Player>;
+  }
+}
+export default meta;
 
-const Template: ComponentStory<typeof Player> = (args) => <div>
-  <Player {...args} />
-</div>;
+type Story = StoryObj<typeof Player>;
 
-export const Controls = Template.bind({});
-Controls.args = {
-  controls: true,
-  streamUrl: 'wss://vd1.wmspanel.com/video_demo_without_ads/stream'
+export const Controls: Story = {
+  args: {
+    controls: true,
+    streamUrl: 'wss://vd1.wmspanel.com/video_demo_without_ads/stream'
+  }
 };
 
-export const AutoplayMuted = Template.bind({});
-AutoplayMuted.args = {
-  controls: false,
-  autoplay: true,
-  muted: true,
-  streamUrl: 'wss://vd1.wmspanel.com/video_demo_without_ads/stream'
+export const AutoplayMuted: Story = {
+  args: {
+    controls: false,
+    autoplay: true,
+    muted: true,
+    streamUrl: 'wss://vd1.wmspanel.com/video_demo_without_ads/stream'
+  }
 }
 
-export const iPhoneFallback = Template.bind({});
-iPhoneFallback.args = {
-  width: '300px',
-  height: 'auto',
-  controls: false,
-  autoplay: true,
-  muted: true,
-  streamUrl: 'wss://vd1.wmspanel.com/video_demo_without_ads/stream; https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8'
+export const iPhoneFallback: Story = {
+  args: {
+    width: '300px',
+    height: 'auto',
+    controls: false,
+    autoplay: true,
+    muted: true,
+    streamUrl: 'wss://vd1.wmspanel.com/video_demo_without_ads/stream; https://demo.pgstreaming.pg.company:1444/live/democh1en.m3u8'
+    // https://demo.pgstreaming.pg.company:1444/live/democh1en.m3u8
+  }
 }
